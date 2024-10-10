80275dd0c385f3db53d3c3134a28dce1eb082078
mariana zimpel <142227275+marianaazimpel@users.noreply.github.com>
2024-10-10 14:09:02 -0300

    commit

---
+++ assents/perguntas.js
@@ -0,0 +1,11 @@
+criacartao(
+    categoria, pergunta ,resposta
+)
+
+criacartao(
+    'programação','o que é javascript', 'o javascript é uma ligação de fraça é paris'
+)
+
+criacartao(
+    'programação', 'o que é uma função', 'uma função é um bloco de codigo que executa alguma tarefa'
+)
