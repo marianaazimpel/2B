964e983037d5240bbc203a1fac6c1265dd76cdf7
mariana zimpel <142227275+marianaazimpel@users.noreply.github.com>
2024-10-17 13:58:13 -0300

    commit

---
+++ app.js
@@ -0,0 +1,20 @@
+function criacartao(categoria, pergunta, resposta){
+    console.log(categoria, pergunta,resposta);
+}
+
+function criacartao(categoria, pergunta,resposta){
+    let container= document.getElementById('container');
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+    cartao.innerHTML = `<div class="cartao_conteudo"> 
+    <h3> ${categoria} </h3>
+    <div class ="cartao_contudo_pergunta">
+    <p> ${categoria} </p>
+    </div>
+    <div class="cartao_conteudo_resposta">
+    <p>  ${categoria}
+    </p>
+    </div>
+    </div>`
+    container.appendchild(cartao)
+    }
--- assents/perguntas.js
+++ assents/perguntas.js
@@ -9,3 +9,9 @@ criacartao(
 criacartao(
     'programação', 'o que é uma função', 'uma função é um bloco de codigo que executa alguma tarefa'
 )
+
+criacartao(
+    'lingua inglesa'
+    'como se diz oi em ingles'
+    'oi em ingles se diz hi (rai)'
+)
